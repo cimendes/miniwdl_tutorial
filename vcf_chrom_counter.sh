e3b26de52bc872436535c2ffc6d6ba4f58c82bca
cimendes <iines.mendes@gmail.com>
2023-02-06 14:39:57 +0000

    add wld workflow with scatter step

---
+++ vcf_chrom_counter.sh
@@ -0,0 +1,6 @@
+```bash
+miniwdl run 2a_vcf_chrom_counter.wdl \
+  vcf_gz=https://ftp-trace.ncbi.nlm.nih.gov/ReferenceSamples/giab/release/NA12878_HG001/latest/GRCh38/HG001_GRCh38_1_22_v4.2.1_benchmark.vcf.gz \
+  tbi=https://ftp-trace.ncbi.nlm.nih.gov/ReferenceSamples/giab/release/NA12878_HG001/latest/GRCh38/HG001_GRCh38_1_22_v4.2.1_benchmark.vcf.gz.tbi \
+  --verbose
+```
